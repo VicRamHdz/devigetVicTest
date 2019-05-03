7780d6e0017809c5fdb49d2a94cf21c8af40f72c
Victor Manuel Ramos Hernandez <>
2019-05-03 00:03:42 -0500

    Adding swipe capability to display and hide menu

--- RedditPost/RedditPost/Views/MainPage.xaml.cs
+++ RedditPost/RedditPost/Views/MainPage.xaml.cs
@@ -15,10 +15,72 @@ namespace RedditPost.Views
     [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
+        private bool IsSwiping = false;
+
         public MainPage()
         {
             InitializeComponent();
             this.BindingContext = new MainPageViewModel();
+
+            var SwipeDetail = new SwipeGestureRecognizer { Direction = SwipeDirection.Right };
+            SwipeDetail.Swiped += OnSwipedDetail;
+            this.stcDetails.GestureRecognizers.Add(SwipeDetail);
+
+            var SwipeLeftDetail = new SwipeGestureRecognizer { Direction = SwipeDirection.Left };
+            SwipeLeftDetail.Swiped += OnSwipedLeftDetail;
+            this.stcDetails.GestureRecognizers.Add(SwipeLeftDetail);
+
+            var TapDetail = new TapGestureRecognizer();
+            TapDetail.Tapped += OnTapDetail;
+            this.stcDetails.GestureRecognizers.Add(TapDetail);
+
+            var SwipeMenuLeft = new SwipeGestureRecognizer { Direction = SwipeDirection.Left };
+            SwipeMenuLeft.Swiped += OnSwipedMenu;
+            this.Menu.GestureRecognizers.Add(SwipeMenuLeft);
+        }
+
+        void OnSwipedDetail(object sender, SwipedEventArgs e)
+        {
+            IsSwiping = true;
+            switch (e.Direction)
+            {
+                case SwipeDirection.Right:
+                    Menu.IsVisible = true;
+                    break;
+            }
+            IsSwiping = false;
+        }
+
+        void OnSwipedLeftDetail(object sender, SwipedEventArgs e)
+        {
+            IsSwiping = true;
+            switch (e.Direction)
+            {
+                case SwipeDirection.Left:
+                    Menu.IsVisible = false;
+                    break;
+            }
+            IsSwiping = false;
+        }
+
+        void OnTapDetail(object sender, EventArgs e)
+        {
+            IsSwiping = true;
+            Menu.IsVisible = false;
+            IsSwiping = false;
+        }
+
+        void OnSwipedMenu(object sender, SwipedEventArgs e)
+        {
+            IsSwiping = true;
+            switch (e.Direction)
+            {
+                case SwipeDirection.Left:
+                    // Handle the swipe
+                    Menu.IsVisible = false;
+                    break;
+            }
+            IsSwiping = false;
         }
 
         protected override void OnAppearing()
@@ -37,6 +99,8 @@ namespace RedditPost.Views
             if (Width > height) //landscape
             {
                 this.mainGrid.Margin = new Thickness(0);
+                if (!IsSwiping)
+                    Menu.IsVisible = true;
             }
             else //portrait
             {
@@ -44,6 +108,8 @@ namespace RedditPost.Views
                 {
                     this.mainGrid.Margin = new Thickness(0, 20, 0, 0);
                 }
+                if (!IsSwiping)
+                    Menu.IsVisible = false;
             }
         }
     }
